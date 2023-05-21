2b7be1631ff8a7dbe282dca96561576c53732313
Basavaraja <basavarajag18@gmail.com>
2023-05-21 12:59:41 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 # Declaring variables for setup functions
 PROJECT_NAME= "housing predictor"
-VERSION= "0.0.1"
+VERSION= "0.0.2"
 AUTHOR="Basava"
 DESCRIPTION="This is a first FSDS Nov batch Mahine Learning Project"
 PACKAGES= ["housing"]
 REQUIREMENT_FILE_NAME= "requirements.txt"
 
 
-def get_requirements_list():
+def get_requirements_list()->List[str]:
     
     """
     Description: This function is going to return a list of requirements
@@ -20,14 +20,14 @@ def get_requirements_list():
     of libraries mentioned in the requirement.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
 name=PROJECT_NAME,
 version= VERSION,
 Author= AUTHOR,
 description= DESCRIPTION,
-packages= PACKAGES,
+packages= find_packages(), #['housing']
 install_requires= get_requirements_list()
 )
 
